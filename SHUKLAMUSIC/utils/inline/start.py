261d3723451f16008f9144e1fa9331e15834302c
XFLSWAN <158449266+XFLSWAN@users.noreply.github.com>
2024-12-26 16:09:56 +0530

    Update start.py

--- SHUKLAMUSIC/utils/inline/start.py
+++ SHUKLAMUSIC/utils/inline/start.py
@@ -30,7 +30,7 @@ def private_panel(_):
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
+            InlineKeyboardButton(text="OWNER", web_app=WebAppInfo(url="site_link")),
         ],
     ]
     return buttons
